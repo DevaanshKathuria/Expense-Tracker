9da117e93b8186980894a9e029bf19517199fd66
DevaanshKathuria <devaanshkathuria@gmail.com>
2026-02-01 11:34:04 +0530

    Created basic file structure and wrote app.ts and server.ts

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+// import expenseRoutes from "./routes/expense.routes";
+
+
+
+class App {
+    app: Application;
+
+    constructor() {
+        this.app = express();
+        this.initializeMiddlewares();
+        // this.initializeRoutes();
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(express.json());
+    }
+
+    // private initializeRoutes() {
+    //     this.app.use("/expense", expenseRoutes)
+    // }
+}
+
+
+export default new App().app;
